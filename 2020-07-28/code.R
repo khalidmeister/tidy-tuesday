0b4ffde133009c705fe2e931f1f20d8a035690c5
khalidmeister <khaliddotdev@gmail.com>
2020-07-28 22:32:51 +0700

    Final For this week

--- 2020-07-28/code.R
+++ 2020-07-28/code.R
@@ -14,55 +14,68 @@ glimpse(penguins)
 summary(penguins)
 glimpse(penguins_raw)
 
-p1 <- ggplot(penguins, aes(x=body_mass_g, fill=species)) +
-  geom_histogram(position = position_identity(),
-                 alpha = .75,
-                 bins = 25) +
-  scale_fill_manual(values = c("#e8505b", "#f9d56e", "#14b1ab")) +
-  theme_minimal() +
-  labs(x = "Body Mass (g)",
-       y = "",
-       fill = "Species",
-       title = "The Distribution of Body Mass in g",
-       subtitle = "For each Palmer Penguins species.",
-       caption = "Created by Irfan Khalid (@MixtureModels)") +
-  theme(
-    legend.position = c(.95, .95),
-    legend.justification = c("right", "top"),
-    legend.box.just = "right",
-    legend.margin = margin(6, 6, 6, 6),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    plot.background = element_rect(fill = "#f1f3f4"),
-    plot.title = element_text(size = 18, face = "bold"),
-    plot.subtitle = element_text(size = 12, face = "italic"),
-    plot.caption = element_text(size = 8)
-  )
-p1
+p1 <- penguins %>%
+  drop_na() %>%
+  ggplot(aes(x=body_mass_g, fill=species)) +
+    geom_histogram(position = position_identity(),
+                   alpha = .75,
+                   bins = 25) +
+    scale_fill_manual(values = c("#e8505b", "#f9d56e", "#14b1ab")) +
+    theme_minimal() +
+    labs(x = "Body Mass (g)",
+         y = "",
+         fill = "Species",
+         title = "The Distribution of Body Mass in g",
+         caption = "Created by Irfan Khalid (@MixtureModels)") +
+    theme(
+      legend.position = c(.95, .95),
+      legend.justification = c("right", "top"),
+      legend.box.just = "right",
+      legend.margin = margin(6, 6, 6, 6),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      plot.background = element_rect(fill = "#f1f3f4", color = "#f1f3f4"),
+      plot.title = element_text(size = 12),
+      plot.caption = element_text(size = 8)
+    )
 
-p2 <- ggplot(penguins, aes(x=bill_length_mm, y=bill_depth_mm, color=species, shape=species)) +
-  geom_point(size = 1.5) +
-  scale_fill_manual(values = c("#e8505b", "#f9d56e", "#14b1ab")) +
-  theme_minimal() +
-  labs(x = "Bill Length (mm)",
-       y = "Bill Depth (mm)",
-       color = "Species",
-       shape = "Species",
-       title = "The Scatter Plot of Palmers Penguins Dataset",
-       subtitle = "Bill Length (mm) vs Bill Depth (mm)",
-       caption = "Created by Irfan Khalid (@MixtureModels)") +
-  theme(
-    legend.position = c(.95, .95),
-    legend.justification = c("right", "top"),
-    legend.box.just = "right",
-    legend.margin = margin(6, 6, 6, 6),
-    panel.grid.major = element_blank(),
-    # panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    plot.background = element_rect(fill = "#f1f3f4"),
-    plot.title = element_text(size = 16, face = "bold"),
-    plot.subtitle = element_text(size = 12),
-    plot.caption = element_text(size = 8)
-  )
+p2 <- penguins %>%
+  drop_na() %>%
+  ggplot(aes(x=bill_length_mm, y=bill_depth_mm, color=species, shape=species)) +
+    geom_point(size = 1.5) +
+    scale_fill_manual(values = c("#e8505b", "#f9d56e", "#14b1ab")) +
+    theme_minimal() +
+    labs(x = "Bill Length (mm)",
+         y = "Bill Depth (mm)",
+         color = "Species",
+         shape = "Species",
+         title = "Bill Length (mm) vs Bill Depth (mm)",
+         caption = "Created by Irfan Khalid (@MixtureModels)") +
+    theme(
+      legend.position = c(.95, .95),
+      legend.justification = c("right", "top"),
+      legend.box.just = "right",
+      legend.margin = margin(6, 6, 6, 6),
+      panel.grid.major = element_blank(),
+      # panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      plot.background = element_rect(fill = "#f1f3f4", color = "#f1f3f4"),
+      plot.title = element_text(size = 12),
+      plot.caption = element_text(size = 8)
+    )
+
+p1
 p2
+
+install.packages("patchwork")
+library(patchwork)
+p1 + p2 + plot_annotation(
+  title = "The penguins at the Palmer Station, Antarctica",
+  subtitle = "Showing for each Species. Adelie, Gentoo, Chinstrap",
+  theme = theme(
+    plot.background = element_rect(fill ="#f1f3f4", color = "#f1f3f4"),
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 18),
+    plot.subtitle = element_text(hjust = 0.5, size = 12)
+  )
+)
--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Hello! Welcome to my repository on compilation of my work on #TidyTuesday event.
 ## [2020-07-21 => RSPCA Complaints and Outcomes](https://github.com/khalidmeister/tidy-tuesday/tree/master/2020-07-21)
 
 ## [2020-07-28 => Palmers Penguins](https://github.com/khalidmeister/tidy-tuesday/tree/master/2020-07-28)
-![First Plot](2020-07-28/plot_1.png)
-![First Plot](2020-07-28/plot_2.png)
+![Plot For Penguins](2020-07-28/plot_final.png)
